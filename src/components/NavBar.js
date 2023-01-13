f2431781b4fca312bbe1e64c8b38713a186a44f5
Samueelx <gsammygitau@gmail.com>
2023-01-13 21:55:33 +0300

    Fixed error: href attiribute of each link now contains correct value.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-  const linkElements = links.map( (link) => {
-    return <a key={link} href="#home">{link}</a>
+  const linkElements = links.map( (link, index) => {
+    let href = '#' + links[index];
+    return <a key={link} href={href}>{link}</a>
   });
 
   return (
